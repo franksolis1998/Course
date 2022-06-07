fef977104d4aa74a52515695178246fd366e5cde
franksolis1998 <75344673+franksolis1998@users.noreply.github.com>
2022-06-07 10:42:34 -0500

    Add files via upload

---
+++ day 8/notes/notes1.py
@@ -0,0 +1,28 @@
+# def greet():
+#     print("Hello")
+#     print("How do you do?")
+#     print("Isn't the weather nice today?")
+
+# greet()
+
+
+# def greet_with_name(name):
+#      print(f"Hello {name}")
+#      print(f"How do you do {name}?")
+     
+# greet_with_name("Rafy")
+
+
+
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("Frank", "Nowhere")
+
+
+
+
+greet_with(location="Houston", name="Frank")
---
+++ day 9/notes.py/notes.py
@@ -0,0 +1,41 @@
+# programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+
+# # print(programming_dictionary["Bug"])
+
+# programming_dictionary["loop"] = "The action of doing somthing over and over again."
+# print(programming_dictionary)
+
+# empty_dictionary = {}
+
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+
+# Nesting
+captials = {
+    "France": "Paris",
+    "Germany": "Berlin"
+}
+
+# Nesting a list in a Dictionary
+
+# travel_log = {
+#     "France": {"cities_visited"}: ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#     "Germany":{"cities_visited"}: ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+# }
+
+# # Nesting dictionary in a list
+
+# travel_log = [
+#     {
+#         "country": "France",
+#         "cities_visited": ["Paris", "Lille", "Dijion"],
+#         "total_visits": 12
+#     }
+#     {
+#         "country": "Germany",
+#         "cities_visited": ["Berlin", "Hamburg", "Struttgart"],
+#         "total_visits": 5
+#     }
+# ]
