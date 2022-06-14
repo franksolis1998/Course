4d0ae78ae1f2bc1ec4a6e70b2062eb4607bd32b0
franksolis1998 <75344673+franksolis1998@users.noreply.github.com>
2022-06-13 22:11:41 -0500

    Add files via upload

---
+++ day 17/notes.py
@@ -0,0 +1,20 @@
+
+
+
+
+        # Attibute
+# class Car:
+#   def __init__(self, seats):
+#       self.seats = seats
+
+# my_car = Car(5)
+# or 
+# my_car.seats = 5
+
+
+        # Method
+# class Car:
+    # def enter_race_mode():
+        # self.seats = 2 
+
+# Mmy_car.enter_race_mode()
